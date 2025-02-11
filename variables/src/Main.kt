fd3d73e7ab3aa53b3e608578f400375da95dd334
Rabab <raaa555@outlook.com>
2025-02-11 18:57:11 +0300

    Added exercise for mutable vs immutable variabkes

---
+++ variables/src/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+
+    //var name: String = "Rabab"
+
+    //var age: Int = 27
+
+    //println("my name is $name and I am $age years old")
+
+    var score: Int = 10
+    println("Initial score: $score")
+
+    //update socre to 20
+
+    score = 20
+    println("updated score: $score")
+
+    val greeting: String = "Hello, Kotlin!"
+
+
+}
