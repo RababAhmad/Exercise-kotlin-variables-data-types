4258b71eab0dc58cbd15fc622446e096af05861b
Rabab <raaa555@outlook.com>
2025-02-11 19:55:08 +0300

    Added data type conversion exercise

---
+++ variables/src/data_conversion.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+
+    val numString = "100"
+    val numInt = numString.toInt()
+
+    println("numInt is: $numInt")
+
+    val numInt2 = 50
+    val numString2 = numInt2.toString()
+    println("numInt2 as String: $numString2")
+
+}
