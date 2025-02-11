b441df63edb6fd892f91ed6d9d6fb69b425ca0b5
Rabab <raaa555@outlook.com>
2025-02-11 20:17:27 +0300

    Added kotlin data types exercise

--- variables/src/Main.kt
+++ variables/src/Main.kt
@@ -1,21 +1,21 @@
 fun main() {
 
 //task1
-    //var name: String = "Rabab"
+    var name: String = "Rabab"
 
-    //var age: Int = 27
+    var age: Int = 27
 
-    //println("my name is $name and I am $age years old")
+    println("my name is $name and I am $age years old")
 //task 2
-    //var score: Int = 10
-    //println("Initial score: $score")
+    var score: Int = 10
+    println("Initial score: $score")
 
-    //update socre to 20
+    //update socre to 20 //comment
 
-    //score = 20
-    //println("updated score: $score")
+    score = 20
+    println("updated score: $score")
 
-    //val greeting: String = "Hello, Kotlin!"
+    val greeting: String = "Hello, Kotlin!"
 //task3
     val city = "New York"
     println(city)
@@ -25,6 +25,30 @@ fun main() {
 
     println("The type of city is ${city.javaClass.kotlin}")
     println("The type of temperature is ${temperature.javaClass.kotlin}")
+
+
+//task4
+
+
+    val numString = "100"
+    val numInt = numString.toInt()
+
+    println("numInt is: $numInt")
+
+    val numInt2 = 50
+    val numString2 = numInt2.toString()
+    println("numInt2 as String: $numString2")
+
+//task 5
+
+    val numDouble: Double = 36.5
+    println("The temperature is $numDouble degrees")
+
+    val isRaining = false
+    println("is it raining $isRaining")
+
+    val letter: Char = 'R'
+    println("The first letter of my name is $letter")
 
 
 
---
+++ variables/src/data_types.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val numDouble: Double = 36.5
+    println("The temperature is $numDouble degrees")
+
+    val isRaining = false
+    println("is it raining $isRaining")
+
+    val letter: Char = 'R'
+    println("The first letter of my name is $letter")
+
+}
